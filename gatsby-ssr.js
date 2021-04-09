5fc7120bb834cd123d69bd1e76d574bd8cebd769
vpineda7 <vladimir.pineda7@gmail.com>
2021-04-09 02:57:26 -0600

    add theme provider as reset and add custom as global style

---
+++ gatsby-ssr.js
@@ -0,0 +1,27 @@
+import React from "react"
+import {createGlobalStyle, ThemeProvider} from "styled-components"
+import Theme from "./src/themes/theme"
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: border; 
+        margin:0; 
+        padding:0;
+    }
+
+    body, html {
+        font-family: ${props => props.theme.fonts.main};
+        height.100%;
+        backgroud-color: ${props => props.theme.colors.light1};
+    }
+    
+
+`
+
+
+export const wrapRootElement = ({ element }) => (
+    <ThemeProvider theme={Theme}>
+        <GlobalStyle/>
+        {element}
+    </ThemeProvider>
+)
